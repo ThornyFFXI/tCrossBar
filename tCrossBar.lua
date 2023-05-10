cd8afc275f4733e2e1380cd3f1613927f38c2b2a
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2023-05-10 08:59:46 -0400

    fix bug causing invalid commands to change layout

--- tCrossBar.lua
+++ tCrossBar.lua
@@ -21,7 +21,7 @@
 
 addon.name      = 'tCrossBar';
 addon.author    = 'Thorny';
-addon.version   = '1.04';
+addon.version   = '1.05';
 addon.desc      = 'Creates a controller scheme for activating macros, and provides visual aids for your macroed abilities.';
 addon.link      = 'https://ashitaxi.com/';
 
@@ -75,10 +75,6 @@ ashita.events.register('command', 'command_cb', function (e)
     if (#args > 1) and (string.lower(args[2]) == 'palette') then
         gBindings:HandleCommand(args);
         return;
-    end
-
-    if (gInterface ~= nil) then
-        gInterface:Initialize(args[2]);
     end
 end);
 
